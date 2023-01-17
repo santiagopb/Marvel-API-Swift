f0dd497ce7cfc53a41a5206bf1263cc4582c0abc
Santiago Pereira <santiagopb@msn.com>
2023-01-18 00:56:29 +0100

    Marvel Characters - Configure NavigationBar

--- Marvel/modules/characterdetail/view/CharacterDetailViewController.swift
+++ Marvel/modules/characterdetail/view/CharacterDetailViewController.swift
@@ -15,6 +15,28 @@ class CharacterDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        configureNavigation()
         presenter?.viewIsReady()
+    }
+    
+    func configureNavigation() {
+        let buttonItem: UIButton = {
+            let button = UIButton(type: .system)
+            button.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+            button.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+            button.backgroundColor = .white.withAlphaComponent(0.7)
+            button.layer.cornerRadius = 15
+            button.layer.borderWidth = 1
+            button.layer.borderColor = UIColor.black.cgColor
+            return button
+        }()
+        
+        buttonItem.addTarget(self, action: #selector(backToMain), for: .touchUpInside)
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: buttonItem)
+        self.navigationController?.navigationBar.tintColor = .black
+    }
+    
+    @objc func backToMain() {
+        self.navigationController?.popViewController(animated: true)
     }
 }
